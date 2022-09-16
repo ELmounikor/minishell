186582e519fec533abf33862e338f7090f84ec03
Mounia El Koraichi <56978020+ELmounikor@users.noreply.github.com>
2022-09-16 17:49:11 +0100

    Add files via upload

--- Libft/ft_lstsize.c
+++ Libft/ft_lstsize.c
@@ -6,7 +6,7 @@
 /*   By: mounikor <mounikor@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/24 21:02:44 by mel-kora          #+#    #+#             */
-/*   Updated: 2022/09/16 17:19:49 by mounikor         ###   ########.fr       */
+/*   Updated: 2022/09/16 17:46:39 by mounikor         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,7 +32,7 @@ int cmd_count(t_list *lst)
 	count = 0;
 	while (lst)
 	{
-		while (lst && lst->id != 1 && lst->id != 11 && lst->id != 22)
+		while (lst && lst->id != 1)
 			lst = lst->next;
 		count++;
 		if (lst)
@@ -54,7 +54,7 @@ int *cmd_size(t_list *lst)
 	i = 0;
 	while (lst)
 	{
-		while (lst && lst->id != 1 && lst->id != 11 && lst->id != 22)
+		while (lst && lst->id != 1)
 		{
 			if (lst->id % 4 && lst->id % 7 && lst->id % 77)
 				size[i]++;

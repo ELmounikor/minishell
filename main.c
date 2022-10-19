c07514d0002bf30503b8be3159800206caf56d9d
saraennaama <95372205+saraennaama@users.noreply.github.com>
2022-10-19 19:33:18 +0100

    Add files via upload

--- main.c
+++ main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mel-kora <mel-kora@student.42.fr>          +#+  +:+       +#+        */
+/*   By: sennaama <sennaama@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/21 16:53:22 by mel-kora          #+#    #+#             */
-/*   Updated: 2022/10/19 16:20:13 by mel-kora         ###   ########.fr       */
+/*   Updated: 2022/10/19 17:53:04 by sennaama         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,28 @@ void	ft_exit(char **cmd)
 	if (!cmd)
 	{
 		printf("exit\n");
-		exit(g_exit_value);
+		exit(0);
 	}
+}
+
+void	free_cmds(t_cmd **cmd)
+{
+	int	i;
+
+	if (!cmd)
+		return ;
+	i = -1;
+	while (cmd[++i])
+	{
+		ft_split_cleaner(cmd[i]->args);
+		if (cmd[i]->fd[0] > 0)
+			close(cmd[i]->fd[0]);
+		if (cmd[i]->fd[1] > 0)
+			close(cmd[i]->fd[1]);
+		free(cmd[i]);
+	}
+	free(cmd);
+	cmd = NULL;
 }
 
 char	*new_prompt(void)
@@ -46,24 +66,26 @@ char	*new_prompt(void)
 int	main(int ac, char **av, char **en)
 {
 	t_list		*input;
-	t_env		*env;
+	t_data		data;
 	t_cmd		**cmd;
 	char		*s;
 
 	av = NULL;
-	env = get_env(en);
+	data.env = get_env(en);
+	data.pwd = ft_strdup(getenv("PWD"));
 	g_exit_value = 0;
 	history_reloader(ac);
+	//ft_envclear(&(data.env), &free);
 	while (1)
 	{
 		signal(SIGINT, handler_sig);
 		signal(SIGQUIT, handler_sig);
 		s = new_prompt();
-		input = tokenizer(s, 0, 0, env);
-		cmd = cmd_extractor(input, env);
-		// cmdprint(cmd);
+		input = tokenizer(s, 0, 0, data.env);
+		cmd = cmd_extractor(input, data.env);
+		// //cmdprint(cmd);
 		if (cmd)
-			ft_pipe(cmd, cmd_count(input), env);
+			ft_pipe(cmd, cmd_count(input), &data);
 		free_cmds(cmd);
 		ft_lstclear(&input, &free);
 		ft_free(&s);
@@ -71,6 +93,7 @@ int	main(int ac, char **av, char **en)
 		// system("leaks minishell");
 		// printf("\n==================================================\n");
 	}
-	ft_envclear(&env, &free);
+	//ft_envclear(&data.env, &free);
+	//free(data.pwd);
 	return (0);
 }
